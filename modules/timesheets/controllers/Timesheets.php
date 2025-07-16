77877377e8ea1509b567b72bcf1dd601b2cd5dbf
Pawan Kumar <happyk682@gmail.com>
2025-07-16 11:18:39 +0530

    changes

--- modules/timesheets/controllers/Timesheets.php
+++ modules/timesheets/controllers/Timesheets.php
@@ -248,16 +248,16 @@ class timesheets extends AdminController
 				$result = $this->timesheets_model->get_attendance_manual($staffs, $month, $month_year);
 				$data['staff_row_tk'] = $result['staff_row_tk'];
 				// Remove the first element (Admin N360)
-				// unset($data['staff_row_tk'][0]);
+				unset($data['staff_row_tk'][0]);
 
 				// Optional: Reset array keys (if needed)
-				// $data['staff_row_tk'] = array_values($data['staff_row_tk']);
+				$data['staff_row_tk'] = array_values($data['staff_row_tk']);
 				$data['cell_background'] = $result['cell_background'];
 				// Remove the first element (Admin N360)
-				// unset($data['cell_background'][0]);
+				unset($data['cell_background'][0]);
 
 				// // Optional: Reset array keys (if needed)
-				// $data['cell_background'] = array_values($data['cell_background']);
+				$data['cell_background'] = array_values($data['cell_background']);
 			}
 		}
 		$data_lack = [];
