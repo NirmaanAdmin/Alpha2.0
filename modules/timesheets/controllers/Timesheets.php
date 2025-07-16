625ca37318a7cca56e68ef530f62c06bf7383cb2
Pawan Kumar <happyk682@gmail.com>
2025-07-16 08:59:55 +0530

    changes

--- modules/timesheets/controllers/Timesheets.php
+++ modules/timesheets/controllers/Timesheets.php
@@ -246,15 +246,15 @@ class timesheets extends AdminController
 		} else {
 			if ($data['check_latch_timesheet'] == false) {
 				$result = $this->timesheets_model->get_attendance_manual($staffs, $month, $month_year);
-				$data['staff_row_tk'] = $result['staff_row_tk'];
-				// Remove the first element (Admin N360)
-				unset($data['staff_row_tk'][0]);
+				// $data['staff_row_tk'] = $result['staff_row_tk'];
+				// // Remove the first element (Admin N360)
+				// unset($data['staff_row_tk'][0]);
 
-				// Optional: Reset array keys (if needed)
-				$data['staff_row_tk'] = array_values($data['staff_row_tk']);
-				$data['cell_background'] = $result['cell_background'];
-				// Remove the first element (Admin N360)
-				unset($data['cell_background'][0]);
+				// // Optional: Reset array keys (if needed)
+				// $data['staff_row_tk'] = array_values($data['staff_row_tk']);
+				// $data['cell_background'] = $result['cell_background'];
+				// // Remove the first element (Admin N360)
+				// unset($data['cell_background'][0]);
 
 				// Optional: Reset array keys (if needed)
 				$data['cell_background'] = array_values($data['cell_background']);
